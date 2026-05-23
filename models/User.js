743cc48953895b77fcc4686bce0b8a5c35e31d88
kaizenai-Mentor <kaizenaibot@gmail.com>
2026-05-23 02:12:50 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -38,23 +38,24 @@ const UserSchema = new mongoose.Schema({
     expiresAt: { type: Date, default: null }
   },
   tradingStyle: {
-  riskPerTrade: { type: String, default: '' },
-  dailyDrawdown: { type: String, default: '' },
-  tradingEdge: { type: String, default: '' },
-  entryRule: { type: String, default: '' },
-  stopLossRule: { type: String, default: '' },
-  takeProfitRule: { type: String, default: '' },
-  emotionalTriggers: { type: String, default: '' },
-  maxDailyTrades: { type: String, default: '' },
-  markets: { type: String, default: '' },
-  maxPositionSize: { type: String, default: '' }
-},
+    riskPerTrade: { type: String, default: '' },
+    dailyDrawdown: { type: String, default: '' },
+    tradingEdge: { type: String, default: '' },
+    entryRule: { type: String, default: '' },
+    stopLossRule: { type: String, default: '' },
+    takeProfitRule: { type: String, default: '' },
+    emotionalTriggers: { type: String, default: '' },
+    maxDailyTrades: { type: String, default: '' },
+    markets: { type: String, default: '' },
+    maxPositionSize: { type: String, default: '' }
   },
-  rules: [{
-    name: String,
-    value: String,
-    active: { type: Boolean, default: true }
-  }],
+  rules: [
+    {
+      name: String,
+      value: String,
+      active: { type: Boolean, default: true }
+    }
+  ],
   disciplineScore: {
     type: Number,
     default: 0,
