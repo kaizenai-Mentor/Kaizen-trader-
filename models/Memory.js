fa90391d85193643db3f7a02427a4a7a26acacd8
kaizenai-Mentor <kaizenaibot@gmail.com>
2026-05-28 03:43:25 +0100

    Create Memory.js

---
+++ models/Memory.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const MemorySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  sessionData: {
+    type: String,
+    default: ''
+  },
+  response: {
+    type: String,
+    required: true
+  },
+  asset: {
+    type: String,
+    default: ''
+  },
+  sessionScore: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Memory', MemorySchema);
