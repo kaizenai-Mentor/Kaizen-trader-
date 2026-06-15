cfc3a35833bdf39a8f7de88e5f198bd1420a81ec
kaizenai-Mentor <kaizenaibot@gmail.com>
2026-06-15 23:39:24 +0100

    Update Memory.js

--- models/Memory.js
+++ models/Memory.js
@@ -29,3 +29,40 @@ const MemorySchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Memory', MemorySchema);
+
+const mongoose = require('mongoose');
+
+const MemorySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  sessionData: {
+    type: String,
+    default: ''
+  },
+  response: {
+    type: String,
+    required: true
+  },
+  asset: {
+    type: String,
+    default: ''
+  },
+  sessionScore: {
+    type: Number,
+    default: 0
+  },
+  type: {
+    type: String,
+    default: 'journal',
+    enum: ['journal', 'psychology']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Memory', MemorySchema);
