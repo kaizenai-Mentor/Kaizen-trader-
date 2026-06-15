bcc27f58a6b117aa18e3a74796f8c910c18a3617
kaizenai-Mentor <kaizenaibot@gmail.com>
2026-06-15 23:57:29 +0100

    Update Memory.js

--- models/Memory.js
+++ models/Memory.js
@@ -22,36 +22,6 @@ const MemorySchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-const mongoose = require('mongoose');
-
-const MemorySchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  sessionData: {
-    type: String,
-    default: ''
-  },
-  response: {
-    type: String,
-    required: true
-  },
-  asset: {
-    type: String,
-    default: ''
-  },
-  sessionScore: {
-    type: Number,
-    default: 0
-  },
   type: {
     type: String,
     default: 'journal',
